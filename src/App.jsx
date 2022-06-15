10e22c0e057f7d36f5f4698a46be981bd90b907c
Andresp08 <Andresfp29032001@gmail.com>
2022-06-15 17:21:50 -0500

    starting app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import {Route, Routes} from 'react-router-dom';
+import { Box } from '@mui/material'
+
+import './App.css'
+import ExcerciseDetail from './pages/ExcerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <Box width='400px'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/excercise/:id' element={<ExcerciseDetail/>}/>
+      </Routes>
+      <Footer/>
+    </Box>
+  )
+}
+
+export default App
