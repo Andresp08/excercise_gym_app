08ceeaae7f94e0a15851b8848513333d6432cf5a
Andresp08 <Andresfp29032001@gmail.com>
2022-06-17 10:16:10 -0500

    working with rapidApi, fetching results

--- src/components/SearchExcercises.jsx
+++ src/components/SearchExcercises.jsx
@@ -1,8 +1,92 @@
+import { useEffect, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+
+import { excerciseOptions, fetchData } from "../utils/fetchData";
 
 const SearchExcercises = () => {
-  return (
-    <div>SearchExcercises</div>
-  )
-}
+  const [search, setSearch] = useState("");
+  const [excercises, setExcercises] = useState([]);
+  const [bodyParts, setBodyParts] = useState([])
 
-export default SearchExcercises
+  //display categories
+  useEffect(() => {
+    const fetchExcerciseData = async () => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', excerciseOptions)
+
+      setBodyParts(['all', ...bodyPartsData])
+    }
+    fetchExcerciseData();
+  },[])
+  
+
+  const handleSearch = async (e) => {
+    if (search) {
+      const excercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        excerciseOptions
+      );
+
+      const searchedExcercises = excercisesData.filter(
+        (excercise) =>
+          excercise.name.toLowerCase().includes(search) ||
+          excercise.target.toLowerCase().includes(search) ||
+          excercise.equipment.toLowerCase().includes(search) ||
+          excercise.bodyPart.toLowerCase().includes(search)
+      );
+
+      setSearch('');
+      setExcercises(searchedExcercises);
+    }
+  };
+
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Excercises You <br />
+        Should Know
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            input: {
+              fontWeight: "700px",
+              border: "none",
+              borderRadius: "4px",
+            },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#FFF",
+            borderRadius: "40px",
+          }}
+          height="76px"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          placeholder="Search Excercises"
+          type="text"
+        ></TextField>
+        <Button
+          className="search-btn"
+          sx={{
+            bgcolor: "#FF2625",
+            color: "#FFF",
+            textTransform: "none",
+            width: { lg: "175px", xs: "80px" },
+            fontSize: { lg: "20px", xs: "14px" },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
+};
+
+export default SearchExcercises;
